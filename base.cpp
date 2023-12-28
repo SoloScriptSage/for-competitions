c91253e205d384d92bddae322083fcccfbc35d4f
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-12-28 17:17:08 -0600

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
- 
+
 #define f first
 #define s second
 #define ll long long
 #define MAXN 100005
- 
+#define fast ios::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
+#define clear_arr(a) memset(a, 0, sizeof(a));
+
 using namespace std;
- 
+
 int a[MAXN];
- 
+
 void solve(){
+    
+    
     cout << "\n";
-    memset(a, 0, sizeof(a)); 
+    memset(a, 0, sizeof(a));
 }
- 
+
 int main() {
-    ios::sync_with_stdio(NULL);
-    cin.tie(0);
-    cout.tie(0);
- 
+    fast
+
     int t;
     cin >> t;
- 
+
     while (t--) {
         solve();
     }
- 
+
     return 0;
 }
