4bd22df2629474030276a38c3660a373b02da267
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-12-28 01:30:11 -0600

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+#define f first
+#define s second
+#define ll long long
+#define MAXN 100005
+ 
+using namespace std;
+ 
+int a[MAXN];
+ 
+void solve(){
+    cout << "\n";
+    memset(a, 0, sizeof(a)); 
+}
+ 
+int main() {
+    ios::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
