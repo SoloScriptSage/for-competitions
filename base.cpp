27972f92953b084883d2f0383dfc8a9ecaa481a4
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2024-01-02 04:41:17 -0600

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -4,8 +4,14 @@
 #define s second
 #define ll long long
 #define MAXN 100005
+#define pair<ll, ll> pll
 #define fast ios::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
-#define clear_arr(a) memset(a, 0, sizeof(a));
+#define clar(a) memset(a, 0, sizeof(a));
+#define usll unordered_set<ll>
+#define vll(n) vector<ll> n
+#define all(v) v.begin(), v.end()
+#define file_st ifstream in("input.txt"); cin.rdbuf(in.rdbuf());
+#define file_end in.close();
 
 using namespace std;
 
