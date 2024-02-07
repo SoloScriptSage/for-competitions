167d36b4a9091a83672af4e1b526d93cd66abbca
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2024-02-07 08:21:28 -0600

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,39 +1,28 @@
-//      _                _   _           _     _                            _      
-//   __| | ___  __ _  __| | (_)_ __  ___(_) __| | ___   _ __ ___   ___   __| | ___ 
-//  / _` |/ _ \/ _` |/ _` | | | '_ \/ __| |/ _` |/ _ \ | '_ ` _ \ / _ \ / _` |/ _ \
-// | (_| |  __/ (_| | (_| | | | | | \__ \ | (_| |  __/ | | | | | | (_) | (_| |  __/
-//  \__,_|\___|\__,_|\__,_| |_|_| |_|___/_|\__,_|\___| |_| |_| |_|\___/ \__,_|\___|
-                                                                                
+// ┳┓┏┓┏┓┳┓  ┳┳┓┏┓┳┳┓┏┓  ┳┳┓┏┓┳┓┏┓
+// ┃┃┣ ┣┫┃┃  ┃┃┃┗┓┃┃┃┣   ┃┃┃┃┃┃┃┣ 
+// ┻┛┗┛┛┗┻┛  ┻┛┗┗┛┻┻┛┗┛  ┛ ┗┗┛┻┛┗┛
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll,ll> pll;
-
-#define MAXN 100005
+typedef pair<ll, ll> pll;
+#define fast ios::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
+#define INF 1e9
 #define f first
 #define s second
 #define pb push_back
-#define mp make_pair
-#define fast ios::sync_with_stdio(NULL); cin.tie(0); cout.tie(0);
-#define clar(a) memset(a, 0, sizeof(a));
-#define usll unordered_set<ll>
-#define vll(n) vector<ll> n
-#define all(v) v.begin(), v.end()
-#define file_st ifstream in("input.txt"); cin.rdbuf(in.rdbuf());
-#define file_end in.close();
-#define dec(x) printf("%.10f\n", x)
+#define fill(a,val) memset(a, val, sizeof(a));
+#define rep(i,n) for(int i=0;i<n;++i)
+#define for(i,a,b) for(int i = a; i < b; ++i)
+#define sortv(v) sort(v.begin(), v.end())
+#define sortvr(v) sort(v.rbegin(), v.rend())
+#define MOD 1000000007
+#define ADD(a,b) ((a+b) % MOD)
+#define MUL(a,b) ((a*b) & MOD)
 
-int a[MAXN];
-
-void solve(){
-    
-    
-    cout << "\n";
-    memset(a, 0, sizeof(a));
-}
+void solve() {}
 
 int main() {
     fast
